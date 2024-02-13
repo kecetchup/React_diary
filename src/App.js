ec98dff904e1761c3af77aed387b7ea1ba8fd936
kecetchup <yeonchanho0927@naver.com>
2024-02-13 15:58:26 +0900

    diary 날짜에 따라 일기 필터링하기

--- src/App.js
+++ src/App.js
@@ -1,16 +1,126 @@
+import {Routes,Route, Link} from "react-router-dom";
+import React, { useReducer ,useRef, useEffect, useState} from "react";
 import './App.css';
-import { getEmotionImgById } from './util';
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import Edit from "./pages/Edit";
+
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
+const mockData = [
+  {
+    id: "mock1",
+    date : new Date().getTime(),
+    content : "mock1",
+    emotionId: 1,
+  },
+  {
+    id: "mock2",
+    date : new Date().getTime(),
+    content : "mock2",
+    emotionId: 2,
+  },
+  {
+    id: "mock3",
+    date : new Date().getTime(),
+    content : "mock3",
+    emotionId: 3,
+  },
+]
+
+function reducer(state,action){
+  switch (action.type) {
+    case "INIT":{
+      return action.data;
+    }
+    case "CREATE":{
+      return[action.data,...state];
+    }
+    case "UPDATE":{
+      return state.map((it)=> 
+        String(it.id) === String(action.data.id) ? {...action.data} : it
+      );
+    }
+    case "DELETE":{
+      return state.filter((it)=> 
+        String(it.id) !== String(action.targetId));
+    }
+    default :{
+      return state;
+    }
+  }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <img alt="감정1" src={getEmotionImgById(1)}/>
-      <img alt="감정1" src={getEmotionImgById(2)}/>
-      <img alt="감정1" src={getEmotionImgById(3)}/>
-      <img alt="감정1" src={getEmotionImgById(4)}/>
-      <img alt="감정1" src={getEmotionImgById(5)}/>
-    </div>
-  );
+  const [data,dispatch] = useReducer(reducer,[]);
+  const [isDataLoaded, setIsDataLoaded] = useState(false);
+  const idRef = useRef(0);
+
+  useEffect(()=> {
+    dispatch({
+      type: "INIT",
+      data: mockData,
+    });
+    setIsDataLoaded(true);
+  },[]);
+
+  const onCreate = (date, content, emotionId) => {
+    dispatch({
+      type : "CREATE",
+      data : {
+        id: idRef.current,
+        date: new Date(date).getTime(),
+        content,
+        emotionId,
+      },
+    });
+    idRef.current += 1;
+  };
+  const onUpdate = (targetId, date, content, emotionId) => {
+    dispatch({
+      type : "UPDATE",
+      data : {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotionId,
+      },
+    });
+  };
+  const onDelete = (targetId) => {
+    dispatch({
+      type : "DELETE",
+      data : {
+       targetId,
+      },
+    });
+  };
+  if(!isDataLoaded){
+    return <div>데이터를 불러오는 중입니다</div>;
+  }else{
+    return (
+      <DiaryStateContext.Provider value={data}>
+        <DiaryDispatchContext.Provider
+        value={{onCreate,onUpdate,onDelete,}}>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/new" element={<New/>} />
+              <Route path="/diary/:id" element={<Diary/>} />
+              <Route path="/edit/:id" element={<Edit/>} />
+            </Routes>
+            <div>
+              <Link to={"/"}>Home</Link>
+              <Link to={"/new"}>New</Link>
+              <Link to={"/diary"}>Diary</Link>
+              <Link to={"/edit"}>Edit</Link>
+            </div>
+          </div>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
+    );
+  }
 }
 
 export default App;
